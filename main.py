9e7ca5253d28a2495b2c9f792ec1cbbce2b6e5b1
chisnikoff <45393985+chisnikoff@users.noreply.github.com>
2024-09-14 12:06:31 +0700

    7 шагов позади

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+import smtplib
+server = smtplib.SMTP_SSL('smtp.yandex.ru:465')
+login = 'alex.chisnikov'
+password = 'vuxnivvwfvtjwyhw'
+email_from = 'alex.chisnikov@yandex.ru'
+email_to = 'chisnikoff@mail.ru'
 url_name = 'https://dvmn.org/referrals/1nONToZb43Syef1wxtxbL9fnefqHXxTPicDCCv3A/'
 friend_name = 'Игорь'
 my_name = 'Алексей'
-letter = '''\
-From: ivan@yandex.ru
-To: petr@yandex.ru
+title = '''\
+From: alex.chisnikov@yandex.ru
+To: chisnikoff@mail.ru
 Subject: Приглашение!
 Content-Type: text/plain; charset="UTF-8";\n'''
 text = '''
 Привет, {fr}! {my} приглашает тебя на сайт {url}!
 
-%website% — это новая версия онлайн-курса по программированию. 
+{url} — это новая версия онлайн-курса по программированию. 
 Изучаем Python и не только. Решаем задачи. Получаем ревью от преподавателя. 
 
 Как будет проходить ваше обучение на {url}? 
@@ -24,5 +30,10 @@ text = '''
 Регистрируйся → {url}  
 На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл.'''
 text = text.format(url = url_name, fr = friend_name, my = my_name)
-print(letter + text)
+letter = title + text
+letter = letter.encode("UTF-8")
+print(letter)
+server.login(login, password)
+server.sendmail(email_from, email_to, letter)
+server.quit()
 
